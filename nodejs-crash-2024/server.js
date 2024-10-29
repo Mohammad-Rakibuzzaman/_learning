583e49653bb4f0d9465641dc6679c19ee2093595
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-10-30 02:37:24 +0600

    added till setup Simple Routing

--- nodejs-crash-2024/server.js
+++ nodejs-crash-2024/server.js
@@ -1,17 +1,23 @@
 import http from "http";
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const srever = http.createServer((req, res) => {
   //   res.setHeader("Content-Type", "text/html");
   //   res.statusCode = 404;
 
+  console.log(req.url);
+  console.log(req.method);
+
   //we could do upper two in one line
 
-  res.writeHead(500, { "Content-Type": "application/json" });
+  // res.writeHead(500, { "Content-Type": "application/json" });
+  res.writeHead(200, { "Content-Type": "text/html" });
 
   //   res.write("Hello world of server!");
   //   res.end("<h1><i>Hello world of server!</i></h1>");
-  res.end(JSON.stringify({ message: "Server ERROR!" }));
+
+  // res.end(JSON.stringify({ message: "Server ERROR!" }));
+  res.end("<h1>Hello world bro</h1>");
 });
 
 srever.listen(PORT, () => {
