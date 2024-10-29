d1ca934f08ff00afa51b39b445537a7e9794fd9d
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2024-10-29 18:43:39 +0600

    added crash course

---
+++ nodejs-crash-2024/index.js
@@ -0,0 +1,10 @@
+//we can use this as well app.js or server.js
+
+// console.log("hey its me RRRRR");
+// console.log(global);
+// console.log(process);
+
+// const randomNumberGenerator = require("./utils");
+const randomNumberGeneratorsssss = require("./utils");
+
+console.log(`Random Number is: ${randomNumberGeneratorsssss()}`);
---
+++ nodejs-crash-2024/utils.js
@@ -0,0 +1,5 @@
+function randomNumberGenerator() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+module.exports = randomNumberGenerator;
